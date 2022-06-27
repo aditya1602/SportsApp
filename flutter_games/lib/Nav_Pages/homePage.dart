1350b261e2976df414e8a1d6136c54e5fec79f2c
aditya1602 <aditya.p.sailesh@gmail.com>
2022-06-27 17:35:31 +0530

    HomePage Layout

--- flutter_games/lib/Nav_Pages/homePage.dart
+++ flutter_games/lib/Nav_Pages/homePage.dart
@@ -1,7 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,15 +10,15 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
+    TabController _tabcontroller = TabController(length: 4, vsync: this);
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        
+        backgroundColor: Colors.blueAccent,
         actions: [
-          IconButton(     
+          IconButton(
               onPressed: () {
                 ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                     content: Text(
@@ -27,11 +27,11 @@ class _HomePageState extends State<HomePage> {
                 )));
               },
               icon: Icon(Icons.notifications)),
-              SizedBox(width: 11),
+          SizedBox(width: 11),
         ],
       ),
       drawer: Drawer(
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.white,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.start,
@@ -42,6 +42,85 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 20, top: 30),
+            child: Text(
+              "Welcome Back !!",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            height: 17,
+          ),
+          Container(
+              child: Align(
+            alignment: Alignment.centerLeft,
+            child: TabBar(
+                labelPadding: EdgeInsets.only(left: 20, right: 20),
+                controller: _tabcontroller,
+                labelColor: Colors.black,
+                isScrollable: true,
+                tabs: [
+                  Tab(
+                    text: "Featured",
+                  ),
+                  Tab(
+                    text: "Cricket",
+                  ),
+                  Tab(
+                    text: "Badminton",
+                  ),
+                  Tab(
+                    text: "Football",
+                  ),
+                ]),
+          )),
+          Container(
+            margin: EdgeInsets.only(left: 20),
+            height: 250,
+            width: double.maxFinite,
+            child: TabBarView(controller: _tabcontroller, children: [
+              ListView.builder(
+                itemCount: 3,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin: EdgeInsets.only(right: 15, top: 10),
+                    width: 200,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.black),
+                  );
+                },
+              ),
+              Text("2"),
+              Text("3"),
+              Text("4"),
+            ]),
+          ),
+          SizedBox(height: 20),
+          Container(
+            margin: EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Explore More",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text("see more",
+                    style: TextStyle(
+                      fontSize: 20,
+                    )),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
